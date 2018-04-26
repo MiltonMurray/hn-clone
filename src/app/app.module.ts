e6d3d19bf19b49497d3b13c3ac0b2c173260ed97
Milton Murray <miltonmurray@outlook.com>
2018-04-26 17:03:30 -0400

    Fixed Comments, URL links, Comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule } from '@angular/http';
 import {ViewEncapsulation} from '@angular/core';
-import { MomentModule } from 'ngx-moment';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,8 +11,12 @@ import { StoriesComponent } from './stories/stories.component';
 import { FooterComponent } from './footer/footer.component';
 import { ItemComponent } from './item/item.component';
 import{ HackernewsApiService} from './hackernews-api.service';
-import {DomainPipe} from './domain.pipe';
 
+import { ItemCommentsComponent } from './item-comments/item-comments.component';
+import { routing } from './app.routes';
+
+import { CommentComponent } from './comment/comment.component';
+import { CommentTreeComponent } from './comment-tree/comment-tree.component';
 
 
 @NgModule({
@@ -22,14 +26,14 @@ import {DomainPipe} from './domain.pipe';
     StoriesComponent,
     FooterComponent,
     ItemComponent, 
-    DomainPipe
+    ItemCommentsComponent, CommentComponent, CommentTreeComponent
     
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MomentModule
+    routing
   ],
   providers: [HackernewsApiService],
   bootstrap: [AppComponent]
